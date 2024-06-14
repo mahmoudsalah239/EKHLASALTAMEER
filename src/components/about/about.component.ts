758d305a608ac36252db42e8a647bd98bfda381f
mahmoudsalah239 <mamosalah239@gmail.com>
2024-06-15 00:36:04 +0300

    final update

--- src/components/about/about.component.ts
+++ src/components/about/about.component.ts
@@ -1,10 +1,12 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import * as AOS from 'aos';
 import { HRComponent } from '../hr/hr.component';
+import { NgxTypedWriterModule } from 'ngx-typed-writer';
+
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [HRComponent],
+  imports: [HRComponent,NgxTypedWriterModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
--- src/components/slider/slider.component.ts
+++ src/components/slider/slider.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { AfterViewChecked, Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { NgxTypedWriterModule } from 'ngx-typed-writer';
 
 
 import * as AOS from 'aos';
@@ -8,7 +9,7 @@ import * as AOS from 'aos';
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule,RouterLink,NgxTypedWriterModule],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss',
 })
